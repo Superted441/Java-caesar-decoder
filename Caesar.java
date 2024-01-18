db1ea2f79d3278d4f34e0c64b4c9b34738f1bdb5
Superted441 <80529062+Superted441@users.noreply.github.com>
2024-01-18 11:49:09 +0000

    Update Caesar.java

--- Caesar.java
+++ Caesar.java
@@ -1,251 +1,68 @@
-package Caesar;
-
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.regex.*;
 import java.io.File;
-import java.io.FileWriter;
-
-import javax.print.event.PrintJobListener;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class Caesar {
 
-    static String word, search, ciphertext;
-    static int shift, i4, i5, cutoff;
+    static String word, search, b, a;
+    static int shift, cutoff, y;
     static Scanner reader = new Scanner(System.in);
-    static ArrayList<String> List1 = new ArrayList<String>();
-    static ArrayList<String> List2 = new ArrayList<String>();
+    static ArrayList<String> List1 = new ArrayList<String>(); // Have a word match within
+    static ArrayList<String> List2 = new ArrayList<String>(); // Imported word list
+    static ArrayList<String> List3 = new ArrayList<String>(); // Imported full word list
+    static ArrayList<Integer> List4 = new ArrayList<Integer>(); // store space indexes
+    
+    static String output;
+    static boolean Valid;
     // Auto Identifing Ceaser Cipher
-    static void listselect() {
-        System.out.println("Choose min word length filter 2-20");   
+
+
+    static void filescanner(String path) {
         
-        do {
-            cutoff = reader.nextInt();
-        } while (2 > cutoff && cutoff > 20);
-                        // code above gets filter length
-        switch(cutoff) {
-            case 2:
-                try {
-                    File file1 = new File("Words/allwords.txt");
-                    Scanner myobj1 = new Scanner(file1);
-                    while (myobj1.hasNextLine()) {
-                        List2.add(myobj1.nextLine());
-                    }
-                    break;
-                } catch (Exception e) {
-                    
-                    break;
-                }   
-            case 3:
-                try {
-                    File file2 = new File("Words/allminus2letterwords.txt");
-                    Scanner myobj2 = new Scanner(file2);
-                    while (myobj2.hasNextLine()) {
-                        List2.add(myobj2.nextLine());
-                    }
-                    System.out.println(List2);
-                    break; 
-                } catch (Exception e) {
-                    
-                    break;
-                }
-                    
-            case 4:
-                try {
-                    File file3 = new File("Words/4letterwords.txt");
-                    Scanner myobj3 = new Scanner(file3);
-                    while (myobj3.hasNextLine()) {
-                        List2.add(myobj3.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            case 5:
-                try {
-                    File file4 = new File("Words/5letterwords.txt");
-                    Scanner myobj4 = new Scanner(file4);
-                    while (myobj4.hasNextLine()) {
-                        List2.add(myobj4.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            case 6:
-                try {
-                    File file5 = new File("Words/6letterwords.txt");
-                    Scanner myobj5 = new Scanner(file5);
-                    while (myobj5.hasNextLine()) {
-                        List2.add(myobj5.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            case 7:
-                try {
-                    File file6 = new File("Words/7letterwords.txt");
-                    Scanner myobj6 = new Scanner(file6);
-                    while (myobj6.hasNextLine()) {
-                        List2.add(myobj6.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            case 8:
-                try {
-                    File file7 = new File("Words/8letterwords.txt");
-                    Scanner myobj7 = new Scanner(file7);
-                    while (myobj7.hasNextLine()) {
-                        List2.add(myobj7.nextLine());  
-                    }   
-                } catch (Exception e) {
-                    
-                }
-            case 9:
-                try {
-                    File file8 = new File("Words/9letterwords.txt");
-                    Scanner myobj8 = new Scanner(file8);
-                    while (myobj8.hasNextLine()) {
-                        List2.add(myobj8.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            case 10:
-                try {
-                    File file9 = new File("Words/10letterwords.txt");
-                    Scanner myobj9 = new Scanner(file9);
-                    while (myobj9.hasNextLine()) {
-                        List2.add(myobj9.nextLine());
-                    } 
-                } catch (Exception e) {
-                    
-                }
-
-            case 11:
-                try {
-                    File file11 = new File("Words/11letterwords.txt");
-                    Scanner myobj11 = new Scanner(file11);
-                    while (myobj11.hasNextLine()) {
-                        List2.add(myobj11.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-            case 12:
-                try {
-                    File file22 = new File("Words/12letterwords.txt");
-                    Scanner myobj22 = new Scanner(file22);
-                    while (myobj22.hasNextLine()) {
-                        List2.add(myobj22.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-            case 13:
-                try {
-                    File file33 = new File("Words/13letterwords.txt");
-                    Scanner myobj33 = new Scanner(file33);
-                    while (myobj33.hasNextLine()) {
-                        List2.add(myobj33.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            case 14:
-                try {
-                    File file44 = new File("Words/14letterwords.txt");
-                    Scanner myobj44 = new Scanner(file44);
-                    while (myobj44.hasNextLine()) {
-                        List2.add(myobj44.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            case 15:
-                try {
-                    File file55 = new File("Words/15letterwords.txt");
-                    Scanner myobj55 = new Scanner(file55);
-                    while (myobj55.hasNextLine()) {
-                        List2.add(myobj55.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            case 16:
-                try {
-                    File file66 = new File("Words/16letterwords.txt");
-                    Scanner myobj66 = new Scanner(file66);
-                    while (myobj66.hasNextLine()) {
-                        List2.add(myobj66.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            case 17:
-                try {
-                    File file77 = new File("Words/17letterwords.txt");
-                    Scanner myobj77 = new Scanner(file77);
-                    while (myobj77.hasNextLine()) {
-                        List2.add(myobj77.nextLine());
-                    } 
-                } catch (Exception e) {
-                    
-                }
-                
-            case 18:
-                try {
-                    File file88 = new File("Words/18letterwords.txt");
-                    Scanner myobj88 = new Scanner(file88);
-                    while (myobj88.hasNextLine()) {
-                        List2.add(myobj88.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            case 19:
-                try {
-                    File file99 = new File("Words/19letterwords.txt");
-                    Scanner myobj99 = new Scanner(file99);
-                    while (myobj99.hasNextLine()) {
-                        List2.add(myobj99.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            case 20:
-                try {
-                    File file00 = new File("Words/20letterwords.txt"); 
-                    Scanner myobj00 = new Scanner(file00);
-                    while (myobj00.hasNextLine()) {
-                        List2.add(myobj00.nextLine());
-                    }
-                } catch (Exception e) {
-                    
-                }
-                
-            break;
-
+        try {
+            File file = new File(path);
+            Scanner myobj = new Scanner(file);
+            while (myobj.hasNextLine()) {
+                List2.add(myobj.nextLine());
+            }
+            myobj.close();
+            
+        } catch (Exception e) {
         }
-    } 
-    static void listreader() throws FileNotFoundException {
-        search = List2.get(i5);
-        i5++;
+        
     }
-    static void CaesarCipher(int shift) {
-        ciphertext = ""; // Variable for altered text
+
+
+    static void listselect(int cutoff) {
+ 
+        switch(cutoff) {
+            case 2: filescanner("Words/allwords.txt"); break;
+            case 3: filescanner("Words/allminus2letterwords.txt"); break;
+            case 4: filescanner("Words/4letterwords.txt");
+            case 5: filescanner("Words/5letterwords.txt");
+            case 6: filescanner("Words/6letterwords.txt");
+            case 7: filescanner("Words/7letterwords.txt");
+            case 8: filescanner("Words/8letterwords.txt");
+            case 9: filescanner("Words/9letterwords.txt");
+            case 10: filescanner("Words/10letterwords.txt");    
+            case 11: filescanner("Words/11letterwords.txt");
+            case 12: filescanner("Words/12letterwords.txt");
+            case 13: filescanner("Words/13letterwords.txt");
+            case 14: filescanner("Words/14letterwords.txt");
+            case 15: filescanner("Words/15letterwords.txt");
+            case 16: filescanner("Words/16letterwords.txt");
+            case 17: filescanner("Words/17letterwords.txt");
+            case 18: filescanner("Words/18letterwords.txt");
+            case 19: filescanner("Words/19letterwords.txt");
+            case 20: filescanner("Words/20letterwords.txt"); break;
+        }
+    }
+
+
+    static String CaesarCipher(int shift) {
+        String ciphertext = ""; // Variable for altered text
         char alphabet; // Variable for temporary Holding
         for(int i=0; i < word.length();i++)  // Loop
         {
@@ -265,47 +82,97 @@ class Caesar {
              ciphertext = ciphertext + alphabet;
             }
         }
+        return ciphertext;
     }
-    static void Regex1() {
-        Pattern pattern = Pattern.compile(search, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(ciphertext);
-        boolean matchFound = matcher.find();
-        if(matchFound) {
-            try {
-                FileWriter myWriter = new FileWriter("Results.txt");
-                myWriter.write(search + " ");
-                myWriter.write(ciphertext + "\n");
-                List1.add(ciphertext);
-                System.out.println(search);
-                System.out.println(ciphertext);
-                System.out.println(i4);
-                myWriter.close();
-            } catch (IOException e) {
+
+
+    static void Regex1(String text) {
+        for (int x = 0; x < List2.size(); x++) {
+            search = List2.get(x);
+            Pattern pattern = Pattern.compile(search, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(text);
+            boolean matchFound = matcher.find();
+            
+            if(matchFound) {
+                    List1.add(text);
+                    break;
             }
+        }                    
+    }
+
+    static boolean complete = false;
+    static void Identifing(String x) { 
+        try {
+            File file = new File("Compare.txt");
+            Scanner myobj = new Scanner(file);
+            while (myobj.hasNextLine()) {
+                List3.add(myobj.nextLine());
+            }
+            myobj.close();
+            System.out.println(List3);
+        } catch (Exception e) {
+        } 
+        RecursiveSplit(x, 0);
+        spacing(x);
+    }
+
+
+    static void RecursiveSplit(String x, int i5) {
+        if (x.length() == i5) {
+            complete = true;
         }
+        for (int i6 = 0; i6 < List3.size(); i6++) {
+            if (i5 + List3.get(i6).length() <= x.length())
+            {if (List3.get(i6) == x.substring(i5, i5 + List3.get(i6).length())) {
+                List4.add(i5 + List3.get(i6).length());
+                RecursiveSplit(x, i5 + List3.get(i6).length()-1);
+                if (true == complete) {break;}
+                List4.remove(i5 + List3.get(i6).length());
+            }}
+        }
+
     }
-    static void CaesarCollective() {
-        System.out.println("What Phrase would you like to test");
-        word = reader.nextLine().toLowerCase();
-        int x = 0;
-        listselect();
-        System.out.println(List2.size());
-        for (int i2 = 0; i2 < List2.size(); i2++) {
-            try {
-                listreader();
-                i4 = 0;
-                x++;
-            } catch (FileNotFoundException e) {
-            }
-            for (int i3 = 0; i3 < 26; i3++) {
-                CaesarCipher(i4);
-                Regex1();
-                i4++;
-            }
+
+
+    static void spacing(String x) {
+        String a, b;
+        for (int i7 = 0; i7 < List4.size(); i7++) {
+            a = x.substring(0, (List4.get(i7)+i7));
+            b = x.substring((List4.get(i7)+i7));
+            x = a + " " + b; 
+            
         }
         System.out.println(x);
-        System.out.println(List1);
     }
+    static String input() {
+        System.out.println("What Phrase would you like to test");
+        return reader.nextLine().toLowerCase();
+    }
+
+    
+    static int input2() {
+        System.out.println("Choose min word length filter 2-20");
+        return reader.nextInt();
+    }
+
+
+    static void CaesarCollective() {
+        word = input();
+        listselect(input2());
+
+        for (int i3 = 0; i3 < 26; i3++) {  
+            Regex1(CaesarCipher(i3));
+        }
+
+        System.out.println("done");
+        System.out.println(List1);
+
+        for (int i4 = 0; i4 < List1.size(); i4++) {
+            Identifing(List1.get(i4));
+
+        }
+    }
+    
 
     public static void main(String[] args) {
         CaesarCollective();
